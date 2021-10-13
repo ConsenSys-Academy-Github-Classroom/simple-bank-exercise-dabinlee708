c2bde972887e4c569224052ba1175dc65a34b3cf
Dabin Lee <dabin.lee@outlook.com>
2021-10-13 08:42:12 +0800

    All exercises passed

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -32,11 +32,11 @@ contract SimpleBank {
     event LogEnrolled(address accountAddress);
 
     // Add 2 arguments for this event, an accountAddress and an amount
-    event LogDepositMade();
+    event LogDepositMade(address accountAddress, uint amount);
 
     // Create an event called LogWithdrawal
     // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance 
-    event LogWithdrawal();
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
     /* Functions
      */
@@ -52,10 +52,11 @@ contract SimpleBank {
 
     /// @notice Get balance
     /// @return The balance of the user
-    function getBalance() public returns (uint) {
+    function getBalance() public view returns (uint) {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+      return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
@@ -70,11 +71,14 @@ contract SimpleBank {
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
       // 1. Add the appropriate keyword so that this function can receive ether
-    
+      //done
       // 2. Users should be enrolled before they can make deposits
-
+      require(enrolled[msg.sender] == true);
+      balances[msg.sender] += msg.value;
+      emit LogDepositMade(msg.sender, msg.value);
+      return balances[msg.sender];
       // 3. Add the amount to the user's balance. Hint: the amount can be
       //    accessed from of the global variable `msg`
 
@@ -92,12 +96,14 @@ contract SimpleBank {
       // Subtract the amount from the sender's balance, and try to send that amount of ether
       // to the user attempting to withdraw. 
       // return the user's balance.
-
+      require(balances[msg.sender] >= withdrawAmount);
+      balances[msg.sender] -= withdrawAmount;
+      (bool success, ) = msg.sender.call.value(withdrawAmount)("");
+      require(success, "Transfer failed.");
+      emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
       // 1. Use a require expression to guard/ensure sender has enough funds
-
       // 2. Transfer Eth to the sender and decrement the withdrawal amount from
       //    sender's balance
-
       // 3. Emit the appropriate event for this message
     }
 }
